301363ae6906210220c1fd85829c794120f4875f
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-07-12 00:12:53 +0000

    Add singer query.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,22 +6,21 @@ import (
 	"log"
 	"net/http"
 
+	"cloud.google.com/go/spanner"
 	"github.com/bufbuild/connect-go"
 	singerv1 "github.com/danny-yamamoto/go-spanner-example/gen/singer/v1"
 	"github.com/danny-yamamoto/go-spanner-example/gen/singer/v1/singerv1connect"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
+	"google.golang.org/api/iterator"
 )
 
-/*
 var (
-
 	testProjectName  = "testing-project"
 	testInstanceName = "testing-instance"
 	testDatabase     = "test_database"
-
 )
-*/
+
 type SingerServer struct{}
 
 func (s *SingerServer) Singer(
@@ -32,41 +31,44 @@ func (s *SingerServer) Singer(
 	res := connect.NewResponse(&singerv1.SingerResponse{
 		SingerId: req.Msg.SingerId,
 	})
-	/*
-		db := fmt.Sprintf("projects/%s/instances/%s/databases/%s", testProjectName, testInstanceName, testDatabase)
-		client, err := spanner.NewClient(ctx, db)
-		if err != nil {
-			log.Println(err)
-		}
-		defer client.Close()
-		stmt := spanner.Statement{
-			SQL: `SELECT SingerId, FirstName, LastName FROM Singers
+	db := fmt.Sprintf("projects/%s/instances/%s/databases/%s", testProjectName, testInstanceName, testDatabase)
+	client, err := spanner.NewClient(ctx, db)
+	if err != nil {
+		log.Println(err)
+	}
+	defer client.Close()
+	stmt := spanner.Statement{
+		SQL: `SELECT SingerId, FirstName, LastName FROM Singers
 					WHERE LastName = @lastName`,
-			Params: map[string]interface{}{
-				"lastName": "Richards",
-			},
+		Params: map[string]interface{}{
+			"lastName": "Richards",
+		},
+	}
+	iter := client.Single().Query(ctx, stmt)
+	defer iter.Stop()
+	for {
+		row, err := iter.Next()
+		if err == iterator.Done {
+			fmt.Println("iterator.Done:", err)
+			break
 		}
-		iter := client.Single().Query(ctx, stmt)
-		defer iter.Stop()
-		for {
-			row, err := iter.Next()
-			if err != nil {
-				fmt.Println("Failed to iterate:", err)
-			}
-			var singerID int64
-			var firstName, lastName string
-			if err := row.Columns(&singerID, &firstName, &lastName); err != nil {
-				fmt.Println("Failed to Columns fetches:", err)
-			}
-			fmt.Printf("%d %s %s\n", singerID, firstName, lastName)
+		if err != nil {
+			fmt.Println("Failed to iterate:", err)
+			return res, err
 		}
-	*/
+		var singerID int64
+		var firstName, lastName string
+		if err := row.Columns(&singerID, &firstName, &lastName); err != nil {
+			fmt.Println("Failed to Columns fetches:", err)
+			return res, err
+		}
+		fmt.Printf("%d %s %s\n", singerID, firstName, lastName)
+	}
 	res.Header().Set("Greet-Version", "v1")
 	return res, nil
 }
 
 func main() {
-	fmt.Println("hello")
 	mux := http.NewServeMux()
 	singer := &SingerServer{}
 	path, handler := singerv1connect.NewSingerServiceHandler(singer)
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	cloud.google.com/go/spanner v1.47.0
 	github.com/bufbuild/connect-go v1.9.0
+	golang.org/x/net v0.12.0
 	google.golang.org/api v0.130.0
 	google.golang.org/grpc v1.56.2
 	google.golang.org/protobuf v1.31.0
@@ -30,7 +31,6 @@ require (
 	github.com/googleapis/gax-go/v2 v2.11.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
-	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/oauth2 v0.9.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
